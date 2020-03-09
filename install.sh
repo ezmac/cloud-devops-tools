994eee38977cbe0e405457f39c131fa28d26eabb
Tad M <wm284@cornell.edu>
2020-03-09 13:22:30 -0400

    Add check that install directory exists

--- install.sh
+++ install.sh
@@ -2,9 +2,14 @@
 
 install_dir=${1:-$HOME/.local/bin}
 
+if [ ! -d $install_dir ]; then
+  echo "$install_dir did not exist, please check that it does or set your own."
+  exit 1
+fi
+
 for i in `find . -name \*.sh -not -name install.sh`; do
   bin_name=${i%.sh}
   echo $bin_name
-  ln -s $PWD/$i $install_dir/$bin_name
+  ln -fs $PWD/$i $install_dir/$bin_name
 done
 
